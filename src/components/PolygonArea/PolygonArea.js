73f71d7a7503c822b7620abaf662506b0cd35425
Erdal Nayir <19360859049@ogrenci.btu.edu.tr>
2022-12-08 13:24:04 +0300

    Update PolygonArea.js

--- src/components/PolygonArea/PolygonArea.js
+++ src/components/PolygonArea/PolygonArea.js
@@ -2,14 +2,13 @@ import React from "react";
 import { clippedVoronois } from "../../utils/data";
 import { createGeoJsonPolygon } from "../../utils/dataParser";
 import SinglePolygon from "../SinglePolygon/SinglePolygon";
-import { clippedTestData } from "../../utils/data";
 
 function PolygonArea() {
   return clippedVoronois["features"].map((data, index) => {
     var coordinates = data["geometry"]["coordinates"];
     var id = data.id;
     var polygonData = createGeoJsonPolygon(coordinates, id);
-    return <SinglePolygon key={index} data={polygonData} ></SinglePolygon>;
+    return <SinglePolygon key={index} data={polygonData}></SinglePolygon>;
   });
 }
 
